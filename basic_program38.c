66795d5779783f8d66b6d00e3af25b0a504617d0
SAGAR T <ssagu6661@gmail.com>
2024-10-19 14:32:31 +0530

    Add files via upload

---
+++ basic_program38.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    int a[10],i;
+    clrscr();
+    printf("enter a array\n");
+    for (int i = 0; i < =9; i++)
+    {
+      scanf("%d",&a[i]);
+    }
+    printf("Entered array is\n");
+    for (int i = 0; i < =9; i++)
+    {
+        printf("%d\n",a[i]);
+    }
+    getch();
+    
+}
