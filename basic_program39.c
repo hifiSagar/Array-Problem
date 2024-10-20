7531ca616f0e7e5cbf555061af001cb81472983b
SAGAR T <ssagu6661@gmail.com>
2024-10-20 07:23:36 +0530

    Add files via upload

---
+++ basic_program39.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main(){
+int a[2] [2],i,j;
+printf("Enter the elements of array\n");
+
+for ( i = 0; i < = 1; i++)
+{
+    for ( j = 0; j < =1; j++)
+{
+    scanf("%d",&a[i][j]);
+}   
+}
+printf("The enter two array is\n");
+for ( i = 0; i < = 1; i++)
+{
+    for ( j = 0; j < =1; j++)
+{
+    printf("%d",&a[i][j]);
+}  
+printf("\n") ;
+}
+}
